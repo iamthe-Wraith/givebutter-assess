4637bb2dd45ea8ab6beb9b3bfb05fc9d0552769f
iamthe-Wraith <allhailthewraith@protonmail.com>
2023-08-28 16:42:21 -0400

    adds no results found state

--- src/App.js
+++ src/App.js
@@ -94,22 +94,26 @@ function App() {
                 <input value={searchValue} onChange={onSearchValueChange} placeholder={'Search Pokemon'}/>
             </div>
             <div className={'pokedex__content'}>
-                {pokemon.length > 0 && (
-                    <div className={'pokedex__search-results'}>
-                        {
-                            pokemon.map(monster => {
-                                return (
-                                    <div className={'pokedex__list-item'} key={monster.name}>
-                                        <div>
-                                            {monster.name}
-                                        </div>
-                                        <button onClick={onGetDetails(monster.name)}>Get Details</button>
+                <div className={'pokedex__search-results'}>
+                    {
+                        pokemon.length === 0
+                            ? (
+                                <div className={'pokedex__list-item'}>
+                                    <div>
+                                        No Results Found
                                     </div>
-                                )
-                            })
-                        }
-                    </div>
-                )}
+                                </div>
+                            )
+                            : pokemon.map(monster => (
+                                <div className={'pokedex__list-item'} key={monster.name}>
+                                    <div>
+                                        {monster.name}
+                                    </div>
+                                    <button onClick={onGetDetails(monster.name)}>Get Details</button>
+                                </div>
+                            ))
+                    }
+                </div>
                 {
                     fetchingPokemonDetails && (
                         <div className={'pokedex__details pokedex__details-loading'}>
